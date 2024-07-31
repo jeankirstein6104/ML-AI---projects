a2d5ace508a5fa128f3fe32bf22912b472c35cee
jeankirstein6104 <jeankirstein6104@gmail.com>
2024-07-31 15:07:57 +0530

    Delete db-project directory

--- db-project/structure/url.py
+++
@@ -1,7 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb+srv://jeankirstein6104:UaSXsPMMz99Acj9v@basics-projects.buat5ge.mongodb.net/?retryWrites=true&w=majority&appName=basics-projects')
-db = client.flowers
-iris = db.iris_samples
-iris_test = db.iris_testing
-#mongodb+srv://jeankirstein6104:<password>@basics-projects.buat5ge.mongodb.net/?retryWrites=true&w=majority&appName=basics-projects
